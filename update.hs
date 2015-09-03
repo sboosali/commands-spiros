1e97fb08923f4eb354f77772efdd22c326f0466b
Sam Boosalis <samboosalis@gmail.com>
2015-09-02 18:11:28 -0700

    pushFiles

--- update.hs
+++ update.hs
@@ -1,45 +1,80 @@
 #!/usr/bin/env runhaskell
-{-# LANGUAGE OverloadedStrings, DoAndIfThenElse #-}
-import Turtle
--- runhaskell ./update.hs
--- cabal install --user turtle
--- https://hackage.haskell.org/package/turtle
-shell_ cmd      = shell cmd      empty
-proc_  cmd args = proc  cmd args empty
-rsync      args = proc_ "rsync" args
-fp2txt = format fp
---txt2fp = fromString 
+{-# LANGUAGE OverloadedStrings, DoAndIfThenElse, LambdaCase #-} 
+import Turtle hiding (cp,sed)
+import qualified Turtle
+import           GHC.Exts                        (fromString)
+import qualified Data.Text.Lazy                  as T
 
-(-:) = (,)
-bimapPair f g = map (\(a,b) -> (f a, g b))
+-------------------------------------------------------------------------------
+-- config
 
 __file__ = "update.hs"
-idir = "/Users/sboosalis/commands-core"
+idir = "/Users/sboosalis/commands-core/"
+
+-- there are three filepaths per (A,B) pair: the A "there" ($idir/sources/), the A "here" (./sources/), and B (./config/)
+files =
+ ["sources/Commands/Plugins/Example.hs"-: "config/Commands/Plugins/Spiros.hs"
+
+ ,"sources/Commands/Plugins/Example/Root.hs"-: "config/Commands/Plugins/Spiros/Root.hs"
+ ,"sources/Commands/Plugins/Example/Phrase.hs"-:"config/Commands/Plugins/Spiros/Phrase.hs"
+ ,"sources/Commands/Plugins/Example/Shortcut.hs"-: "config/Commands/Plugins/Spiros/Shortcut.hs"
+
+ ,"sources/Commands/Frontends/Dragon13/Shim.hs"-: "config/Commands/Plugins/Spiros/Shim.hs"
+-- ,""-: ""
+ ]
+
+-------------------------------------------------------------------------------
 
 main = do
  chmod rwx __file__
 
- mktree ("./sources/Commands/Plugins/Example")
- rsync [ "-av" --, "--dry-run"
-       , idir<>"/sources/Commands/Plugins/Example/"
-       ,        "sources/Commands/Plugins/Example" ]
- rsync [ "-av" --, "--dry-run"
-       , idir<>"/sources/Commands/Plugins/Example.hs"
-       ,        "sources/Commands/Plugins/Example.hs" ]
- mktree ("sources/Commands/Frontends/Dragon13/")
- rsync [ "-av" --, "--dry-run"
-       , idir<>"/sources/Commands/Frontends/Dragon13/Shim.hs"
-       ,        "sources/Commands/Frontends/Dragon13/Shim.hs" ]
+ -- the user may have forgotten to configure their own directory
+ idirExists <- liftIO$ testdir idir
+ unless idirExists $ do
+  die$ format ("idir ("%fp%") doesn't exist") idir
+
+ -- parse cmdln opts and run
+ opts <- options "Update Script" (arg pOptions "action" "")
+ runOptions opts >>= exit
+
+data Options = PullFiles | PushFiles
+ deriving (Show,Enum)
+
+pOptions = \case
+  "pull" -> Just PullFiles
+  "push" -> Just PushFiles
+  _ -> Nothing
+
+runOptions = \case
+ PullFiles -> pullFiles
+ PushFiles -> pushFiles
+
+-------------------------------------------------------------------------------
+
+pullFiles = do
+
+ sh$ do
+  (file,_) <- select files
+  mktree (directory file)
+  cp (idir<>file) file
+
+ sh$ do
+  (source,config) <- select files
+  mktree (directory config)
+  cp source config
+
  echo ""
+ echo "[find sources]"
  view$ find chars "sources"
 
  odir <- pwd
- let filesAbs = map (\(fileA,fileB) -> (odir<>fileA, odir<>fileB)) filePairs
+ let filesAbs = map (\(fileA,fileB) -> (odir<>fileA, odir<>fileB)) files
  testEdiffFiles filesAbs
  echo ""
+ echo "M-x eval-last-sexp"
  echoEdiffFiles filesAbs
 
- return ()
+ return ExitSuccess
 
 testEdiffFiles
  = traverse testfileDie
@@ -49,7 +84,7 @@ testfileDie file = do
  exists <- testfile file
  if   exists
  then return()
- else die$ format ("file doesn't exist: "%fp) file
+ else die$ format ("[testfile] file doesn't exist ("%fp%")") file
 
 echoEdiffFiles
  = traverse echo
@@ -61,14 +96,67 @@ ediff_files fileA fileB = format ("(ediff-files "%s%" "%s%")") (repr fileA) (rep
  -- show the strings for double quotes
  -- escaped double quotes: format ("(ediff-files \""%s%"\" \""%s%"\")") (fileA) (fileB)
 
-filePairs =
- ["sources/Commands/Plugins/Example.hs"-: "config/Commands/Plugins/Spiros.hs"
+-------------------------------------------------------------------------------
 
- ,"sources/Commands/Plugins/Example/Root.hs"-: "config/Commands/Plugins/Spiros/Root.hs"
- ,"sources/Commands/Plugins/Example/Phrase.hs"-:"config/Commands/Plugins/Spiros/Phrase.hs"
- ,"sources/Commands/Plugins/Example/Shortcut.hs"-: "config/Commands/Plugins/Spiros/Shortcut.hs"
+pushFiles = do
 
- ,"sources/Commands/Frontends/Dragon13/Shim.hs"-: "config/Commands/Plugins/Spiros/Shim.hs"
--- ,""-: ""
- ]
+ -- copy to fake "stage"
+ sh$ do
+  (source,config) <- select files
+  mktree (directory source)
+  cp config source
 
+ -- fix imports, rename vars
+ sh$ do
+  (file,_) <- select files
+  sed (has ("Spiros" *> return "Example")) (input file)
+  sed (has ("spiros" *> return "example")) (input file)
+
+ -- don't backup, expect version control
+ exitCode <- withPWD idir $do
+  git ["--no-pager", "diff", "--exit-code"] .&&. git ["--no-pager", "diff", "--exit-code", "--cached"]
+  --git status --porcelain --untracked-files=no
+ unless (exitCode==ExitSuccess) $ do
+  die$ "[git diff] nonempty"
+
+ sh$ do
+  (file,_) <- select files
+  cp file (idir<>file)
+
+ -- print commit messages, we pseudo-"merge" between two different repos
+ git ["--no-pager", "log", "--pretty=%H%n%n%B", "--all"]
+
+ return ExitSuccess
+
+-------------------------------------------------------------------------------
+-- etc
+
+shell_ cmd      = shell cmd      empty
+proc_  cmd args = proc  cmd args empty
+
+-- cp = Turtle.cp
+cp = \src dst -> echo (format ("cp ("%fp%") ("%fp%")") src dst) *> return ExitSuccess -- dry-run
+
+-- sed = Turtle.sed
+sed = \pat inp -> return ExitSuccess
+
+fp2txt = format fp
+txt2fp = fromString . T.unpack
+
+git args = proc_ "git" args
+
+(-:) = (,)
+bimapPair f g = map (\(a,b) -> (f a, g b))
+
+withPWD newDir mx = do
+ oldDir <- pwd
+ cd newDir
+ x <- mx
+ cd oldDir
+ return x
+
+-- runhaskell ./update.hs
+-- cabal install --user turtle
+-- https://hackage.haskell.org/package/turtle
+
+-------------------------------------------------------------------------------
