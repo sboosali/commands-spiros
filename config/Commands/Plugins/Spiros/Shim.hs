a558fb85109bcd89201d0bbee4491ed28b0bbb3a
Sam Boosalis <samboosalis@gmail.com>
2015-09-18 23:29:00 -0700

    re-factor the shim

--- config/Commands/Plugins/Spiros/Shim.hs
+++ config/Commands/Plugins/Spiros/Shim.hs
@@ -27,18 +27,23 @@ import json
 import urllib2
 import traceback
 
+
+
+
+
+
 # interpolated from "H"askell
 
+H_EXPORT = {__export__}
 H_RULES  = {__rules__}
 H_LISTS  = {__lists__}
-H_EXPORT = {__export__}
 H_SERVER_HOST = {__serverHost__}  
 H_SERVER_PORT = {__serverPort__}
 
 # e.g. for debugging
+# H_EXPORT = 'test'
 # H_RULES  = '''<test> exported = \{test};'''
 # H_LISTS  = \{'test', ['upcase region']}
-# H_EXPORT = 'test'
 # H_SERVER_HOST = "192.168.56.1"
 # H_SERVER_PORT = '8666'
 
@@ -46,15 +51,35 @@ server_address = "http://%s:%s/" % (H_SERVER_HOST, H_SERVER_PORT)
 
 # see handle_microphone(...)
 # "wake up" is somewhat redundant with "mike on", but necessary because when the Dragon microphone puts itself to sleep, only "wake up" will wake it back up, and we handle that recognition to enable any disabled grammars in handle_microphone().
-microphone_rule = '''<microphone> exported = wake up | mike on | mike off | mike dead ;'''
 microphone_export = "microphone"
+microphone_rule = '''<microphone> exported = wake up | mike on | mike off | mike dead ;'''
 
-# see handle_mode(...)
-mode_rule = '''<mode> exported = dictating | commanding;'''
-mode_export = "mode"
+# see handle_dnsmode(...)
+dnsmode_export = "dnsmode"
+dnsmode_rule = '''<dnsmode> exported = dictating | commanding;'''
 
-all_rules   =  microphone_rule  + mode_rule  + H_RULES
-all_exports = [microphone_export, mode_export, H_EXPORT]
+# see handle_correctable(...)
+correctable_export = "correctable"
+correctable_rule = '''<correctable> exported = fix;'''
+
+# see handle_correcting(...)
+correcting_export = "correcting"
+correcting_rule = '''
+<correcting> exported
+ = <dgndictation>
+ | nil | zero | one | two | three | four | five | six | sev | seven | eight | nine;
+'''
+
+active_rules   = '\n'.join([microphone_rule,   dnsmode_rule,   correctable_rule,   H_RULES]) 
+active_exports =           [microphone_export, dnsmode_export, correctable_export, H_EXPORT]
+
+all_rules   = '\n'.join([microphone_rule,   dnsmode_rule,   correctable_rule,   H_RULES,  correcting_rule]) 
+all_exports =           [microphone_export, dnsmode_export, correctable_export, H_EXPORT, correcting_export]
+
+# TODO re-factor the microphone/dnsmode/correct/correcting grammars into their own grammar objects (GrammarBase)
+
+
+
 
 
 # the grammar
@@ -74,32 +99,44 @@ class NarcissisticGrammar(GrammarBase):
 
     gramSpec = None        # initialized in set_rules(...) 
     should_request = True
+    previous_results_object = None        # set by recognition, initialized during first call to gotResultsObject()
+    current_mode = "normal"
 
     def initialize(self):
-        self.set_rules(all_rules, all_exports, exclusive=1)
-        self.set_lists(H_LISTS)
-        self.doOnlyGotResultsObject = True # aborts all processing after calling gotResultsObject
+        self.set_rules  (all_rules)
+        self.set_exports(active_exports, exclusive=1)
+        self.set_lists  (H_LISTS)
+        self.doOnlyGotResultsObject = True   # when True, aborts all processing after calling gotResultsObject
 
     # called when speech is detected before recognition begins.
+    # moduleInfo is just the current window in Windows
     def gotBegin(self, moduleInfo):
         # handleDGNContextResponse(timeit("/context", urlopen, ("%s/context" % server_address), timeout=0.1))
         # TODO parameterize "context" API
 
         print
         print
-        print "-  -  -  -  gotBegin  -  -  -  -"
-        # moduleInfo is just the current window in Windows
+        print
+        print "---------- gotBegin -------------------"
+
 
     def gotHypothesis(self, words):
         print
         print "---------- gotHypothesis -------------"
         print words
 
+
+    # (this callback is where most of the work is done)
+    # 
     # recognitionType = self | reject | other 
     def gotResultsObject(self, recognitionType, resultsObject):
+
         print "---------- gotResultsObject ----------"
         print "recognitionType =", recognitionType
         if not recognitionType: return
+
+        self.previous_results_object = resultsObject
+
         words = next(get_results(resultsObject), [])
         data  = munge_recognition(words)
         url   = "%s/recognition/" % (server_address,)        # TODO parameterize "recognition" API
@@ -116,11 +153,18 @@ class NarcissisticGrammar(GrammarBase):
 #            else:
 #                self.activateSet([microphone_export, H_EXPORT], exclusive=1)
 
-            if should_request(self,data):
+            (should_send_request, should_change_mode) = try_magic_handlers(self,data)
+            self.should_request = False if (should_change_mode == "dnsmode/dictating" or should_change_mode == "correcting") else (True if should_change_mode == "normal" else self.should_request)
+            self.current_mode   = should_change_mode or self.current_mode
+
+            print "current_mode   =", self.current_mode
+            print "should_request =", self.should_request
+            if should_send_request and self.should_request:
                 print 'data  =', json.dumps(data)
                 request  = urllib2.Request(url, json.dumps(data), \{"Content-Type": "application/json"})
                 response = urllib2.urlopen(request)
             pass
+
         except Exception as e:
             print
             print "---------- error ------------------"
@@ -131,8 +175,11 @@ class NarcissisticGrammar(GrammarBase):
         # don't print until the request is sent the response is handled
         try:
             print
-            print "status =", response.getcode()
-            print "body   =", response
+            print "status  =", response.getcode()
+            print "body    =", list(response)
+            for (index, hypothesis) in enumerate(get_results(resultsObject)):
+                print "hypothesis %d = %s" % (index, munge_recognition(hypothesis))
+
         except NameError:
             print
         except Exception as e:
@@ -141,28 +188,32 @@ class NarcissisticGrammar(GrammarBase):
             print e
             print traceback.format_exc()
 
+
     # for debugging only, shows whether specific rules (rather than the generic dgndictation) are matching the recognition
     # not called when (self.doOnlyGotResultsObject=True)
     def gotResults(self, words, fullResults):
         print
-        print "---------- gotResultsObject ----------"
+        print "---------- gotResults ----------"
         print "fullResults =", fullResults
 
+
     # TODO    must it reload the grammar?
-    # TODO    should include export for safety?
-    def set_rules(self, rules, exports, exclusive):
+    def set_rules(self, rules):
         self.gramSpec = rules
         self.load(rules, allResults=1, hypothesis=1)
-        self.set_exports(exports, exclusive)
+
 
     # activateSet is idempotent, unlike activate
     def set_exports(self, exports, exclusive):
         self.activateSet(exports, exclusive=exclusive)
 
+
     # TODO must it reload the grammar?
     def set_lists(self, lists):
         for (lhs, rhs) in lists.items():
             self.setList(lhs, rhs)
+
+
 
 
 
@@ -172,20 +223,38 @@ class NarcissisticGrammar(GrammarBase):
 def handleDGNUpdate(grammar, response):
     pass 
 
+
 # data :: [String] 
 # datum :: String
-def should_request(grammar,data):
-    isRecognitionGood = data and isUnicode(data) and not isNoise(data)
-    if isRecognitionGood:
+def try_magic_handlers(grammar,data):
+
+    # truthy if the recognition is good enough
+    is_recognition_good = data and isUnicode(data) and not isNoise(data)
+
+    # a Maybe String
+    should_change_mode = None
+
+    if is_recognition_good:
         datum = " ".join(data)
-        isRecognitionMagical = handle_abrogation(data) or handle_microphone(grammar,datum) or handle_mode(grammar,datum)
-        shouldRequest = isRecognitionGood and not isRecognitionMagical and grammar.should_request    # redundant for clarity
-        print "should_request =", shouldRequest
-        return shouldRequest
+
+        # truthy if the recognition has been handled "magically"
+        # by the client, and shouldn't be handled by the server TODO make this API explicit in servant
+        should_change_mode = handle_abrogation(data) or handle_correcting(grammar,datum) or handle_correctable(grammar,datum) or handle_microphone(grammar,datum) or handle_dnsmode(grammar,datum)
+    should_send_request = is_recognition_good and not should_change_mode
+
+    return (should_send_request, should_change_mode) 
+
 
 # you can abort any recognition by saying "abrogate". as a rare word, it will rarely be dictated (except for bootstrapping). if you must say it, it supports the "say" prefix (i.e. recognize literally everything after it). 
 def handle_abrogation(data):
-    return data[0] != "say" and "abrogate" in data 
+
+    # TODO the first predicate is a hack to let you say the magic word
+    if data[0] != "say" and "abrogate" in data:
+        return "normal"
+
+    else:
+        return None 
+
 
 # returns true if it matched the recognition (and executed the magic action).
 # in which case, don't send a request to the server to execute any non-magic actions.
@@ -195,32 +264,57 @@ def handle_microphone(grammar,datum):
     if  datum == "wake up" or datum == "mike on":
         # grammar.setMicState("on") 
         grammar.activateSet([microphone_export, H_EXPORT], exclusive=1)
-        return True
+        return "normal"
+
     elif datum == "mike off":
         # grammar.setMicState("sleeping")
         grammar.activateSet([microphone_export],exclusive=1)
-        return True
+        return "microphone/sleeping"       # change state
+
     elif datum == "mike dead":
         # the natlink.setMicState("off") # can't even be manually turned back on via the GUI
-        return True
+        return "microphone/off"       # change state
+
     else:
-        return False
+        return None 
 
-def handle_mode(grammar,datum):
 
-#    print 'handle_mode(', datum, ')'
+def handle_dnsmode(grammar,datum):
+
+#    print 'handle_dnsmode(', datum, ')'
 
     if   datum == "dictating":
-        grammar.activateSet([mode_export],exclusive=0)
+        grammar.activateSet([dnsmode_export],exclusive=0)
         natlinkmain.recognitionMimic(["Dictation","mode"]) 
-        grammar.should_request = False
-        return True
+        return "dnsmode/dictating"          # TODO lol
+
     elif datum == "commanding":
-        grammar.activateSet(all_exports,exclusive=1)
-        grammar.should_request = True
-        return True
+        grammar.activateSet(active_exports,exclusive=1)
+        return "normal"           # TODO lol
+
     else:
-        return False
+        return None
+
+
+def handle_correctable(grammar,datum):
+
+    if   datum == "fix":
+        grammar.activateSet([correcting_export],exclusive=1)
+        return "correcting"     # change state 
+
+    else:
+        return None
+
+
+def handle_correcting(grammar,datum):
+
+    if   datum == "zero":
+        grammar.activateSet(active_exports,exclusive=1)
+        return "normal"      # change state 
+
+    else:
+        return None 
+
 
 def isUnicode(data):
     try:
@@ -232,12 +326,16 @@ def isUnicode(data):
         print traceback.format_exc()
         return False
 
+
 def isNoise(data):
     return data in [["the"],["if"],["him"],["A"],["that"],["a"]] #TODO hack, noise tends to be recognized as these short single words
 
 
 
+
+
 # helpers
+
 
 # current time in milliseconds
 def now():
@@ -246,13 +344,31 @@ def now():
 def first_result(resultsObject):
     return next(get_results(resultsObject), None)
 
+
+# returns an iterator of words (strings)
+# 
+# faster than get_results_verbose, returning only one of the WordInfo fields
 def get_results(resultsObject):
-    '''iterators are more idiomatic'''
     try:
-        for number in xrange(10):
-            yield resultsObject.getWords(number)
+        for choice in xrange(10):
+            yield resultsObject.getWords(choice)
     except:
         return
+
+
+# returns an iterator of the WordInfo's
+# 
+# None means no recognition
+# the first is the recognition 
+# the rest are the hypotheses, which may be used to correct the recognition 
+# there are nine or ten hypotheses at most 
+def get_results_verbose(resultsObject):
+    try:
+        for choice in xrange(10):
+            yield resultsObject.getWordInfo(choice)
+    except:
+        return
+
 
 def munge_recognition(words):
     '''
@@ -261,6 +377,7 @@ def munge_recognition(words):
     '''
     return [word.split('\\\\')[0] for word in words]
 
+
 # http://stackoverflow.com/questions/1685221/accurately-measure-time-python-function-takes
 def timeit(message, callback, *args, **kwargs):
     before = time.clock()
@@ -268,6 +385,26 @@ def timeit(message, callback, *args, **kwargs):
     after = time.clock()
     print message, ': ', (after - before) * 1000, 'ms'
     return result
+
+
+'''
+from natlink.txt:
+
+    wordName - same as returned by ResObj.getChoice
+    cfgParse - same as returned by ResObj.getChoice
+    wordScore - for the first word of the result, this is the score
+        of this choice; lower is better; zero for other words
+    startTime - start of word in milliseconds from start of utterance
+    endTime - end of word in milliseconds from start of utterance
+    engineInfo - integer (actually a DWORD) properties of the word
+    wordPron - first pronunciation for the word; not necessarily
+        the pronunciation which was recognized
+        however, wordPron is always empty, I think to protect intellectual property. 
+
+'''
+class WordInfo(object): pass
+
+
 
 
 
