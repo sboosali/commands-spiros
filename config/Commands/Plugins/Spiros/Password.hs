ee66f24ec86275775292e4cf315760aa0646b034
Sam Boosalis <samboosalis@gmail.com>
2016-01-28 20:16:36 -0800

    (stash)

---
+++ config/Commands/Plugins/Spiros/Password.hs
@@ -0,0 +1,18 @@
+{-| 'password's aren't logged, but otherwise behave identically to 'phrase'. 
+
+TODO hasPassword :: Prism Root Password  
+
+or, safer if you can only use it at the "top level" 
+hasPassword :: Root -> Bool
+
+-}
+module Commands.Plugins.Spiros.Password where 
+
+newtype Password = Password Phrase 
+
+password = 'password <=>
+ Password <$ "password" <*> phrase 
+
+runPassword (Password p) = do
+ nothing 
+
---
+++ config/Commands/Plugins/Spiros/Sustain.hs
@@ -0,0 +1,50 @@
+module Commands.Plugins.Spiros.Sustain where 
+import Commands.Plugins.Spiros.Extra
+
+
+type SustainedMonad = 
+
+data Sustained = Sustained String SustainedMonad SustainedMonad
+
+sustainedGrammar :: Sustained -> R SustainedMonad
+sustainedGrammar (Sustained name begin finish) = 
+ [ "begin"  name -: begin 
+ , "finish" name -: finish 
+ ] 
+
+sustainedSkipping = sustainedGrammar (Sustained "skipping" beginSkipping finishSkipping) 
+ where 
+ beginSkipping = do 
+  
+ finishSkipping = do 
+
+{-|
+
+
+type Sustained name = Tagged name Bool
+
+pattern On = Tagged True
+pattern Off = Tagged False
+
+sustainedGrammar :: proxy name -> R (Sustained name)
+sustainedGrammar (symbolVal -> name) = vocab
+ [ "begin " ++ name -: Tagged True
+ , "finish " ++ name -: Tagged False
+ ]
+
+sustainedDesugar :: (MonadState (Tagged s Bool) m) => m () -> m () -> m ()
+sustainedDesugar begin finish = bool begin finish =<< get 
+
+bool :: a -> a -> Tagged s Bool -> a
+bool begin finish = \case
+ Tagged True -> begin
+ Tagged False -> finish
+
+pSkipping = (Proxy :: Proxy "skipping")
+
+sustainedGrammar pSkipping 
+
+sustainedDesugar pSkipping 
+
+
+-} 
